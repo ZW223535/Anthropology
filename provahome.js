22e2059e1ac44baa58d37166567f44302520f13c
mi-chiamo-michi <michela.chignoli14@gmail.com>
2022-11-27 21:22:15 +0100

    animazioni

--- provahome.js
+++ provahome.js
@@ -1,6 +1,6 @@
 document.querySelectorAll('.cerchi').forEach(element => {
     element.addEventListener('mouseover', (node) => {
         console.log(node.fromElement)
-        node.fromElement.classList.toggle('animate__rubberBand')
+        node.fromElement.classList.toggle('animate__pulse')
     })
 })
