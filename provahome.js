51be0be7ec5f864846808ee4e7d48c8e72e4b2dd
ZW223535 <zeya.wu@mail.polimi.it>
2022-12-21 20:15:43 +0100

    Pus

--- provahome.js
+++ provahome.js
@@ -30,27 +30,3 @@ document.getElementById("schermata2").onclick = function () {
     document.getElementById("schermata2").style.display = "none";
   }
 };
-
-/*var contatore = 1;
-
-document.getElementById("schermata2").onclick = function () {
-  contatore = contatore + 1;
-  console.log(contatore);
-
-  if (contatore == 1) {
-    document.getElementById("scritta1").style.display = "block";
-  }
-
-  if (contatore == 2) {
-    document.getElementById("scritta1").style.display = "none";
-    document.getElementById("scritta2").style.display = "block";
-  }
-
-  if (contatore == 3) {
-    document.getElementById("scritta2").style.display = "none";
-    document.getElementById("scritta3").style.display = "block";
-  }
-
-if (contatore==4) {
-  document.getElementById("schermata2").style.display ="none";
-}}
